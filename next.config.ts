b8f753d07f83e3ab3c7997bb2fc068a1e159df0e
Marat Kafunts <marek83743@outlook.com>
2025-10-01 19:05:21 +0200

    updated next config

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,8 @@
 // next.config.ts
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production";
-const repo = "pizza-app";
-
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
-	output: "export",
-	trailingSlash: true, // Важно для GitHub Pages
-	images: { unoptimized: true },
-	basePath: isProd ? `/${repo}` : "",
-	assetPrefix: isProd ? `/${repo}/` : "",
 };
 
 export default nextConfig;
